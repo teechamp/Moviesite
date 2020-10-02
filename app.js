69bb6722b18879697f303b7e954dce094b9a4dd7
teechamp <temidayosamuelbambo@gmail.com>
2020-10-02 03:23:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btn6 = document.getElementById('btn6');
+let btn7 = document.getElementById('btn7');
+let btn8 = document.getElementById('btn8');
+let btn9 = document.getElementById('btn9');
+let btn10 = document.getElementById('btn10');
+let btn11 = document.getElementById('btn11');
+let btn12 = document.getElementById('btn12');
+let btn13 = document.getElementById('btn13');
+let btn14 = document.getElementById('btn14');
+let btn15 = document.getElementById('btn15');
+let btn16 = document.getElementById('btn16');
+let btn17 = document.getElementById('btn17');
+let btn18 = document.getElementById('btn18');
+let btn19 = document.getElementById('btn19');
+let btn20 = document.getElementById('btn20');
+
+
+function openNewWindow(e,path){
+    e.preventDefault();
+    window.open(path,'_blank',"width=500px,height=500px");
+}
